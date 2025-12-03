d3e960f058e132d95d235d937c56184824a87a7f
Pranay5407 <pranaygurnule28@gmail.com>
2025-12-03 22:25:50 +0530

    Create Contact model with schema definition

---
+++ backend/models /Contact.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    phone: String,
+    address: String,
+    message: String,
+    date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Contact', ContactSchema);
