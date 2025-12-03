30c491a076bfcdc57960806be275ddc1da687ba9
Pranay5407 <pranaygurnule28@gmail.com>
2025-12-03 22:41:13 +0530

    Add WhatsAppButton component for quick access

---
+++ frontend/src/components/WhatsappButon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './WhatsAppButton.css';
+
+const WhatsAppButton = () => (
+    <a href="https://wa.me/918390323024" target="_blank" rel="noreferrer" className="whatsapp_float">
+        <img src="/whatsapp-icon.png" alt="WhatsApp" />
+    </a>
+);
+
+export default WhatsAppButton;
